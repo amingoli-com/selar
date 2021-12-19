9cbefd55def35560f89edc4b50d2d9810d11cd86
amingoli78 <amin.goli78@gmail.com>
2021-12-19 15:40:52 +0330

    create project

---
+++ app/src/main/java/amingoli/com/selar/MainActivity.kt
@@ -0,0 +1,11 @@
+package amingoli.com.selar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
