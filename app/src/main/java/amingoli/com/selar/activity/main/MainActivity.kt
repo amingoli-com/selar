de35eb93b212951e5614b3b67b085138c2f0eb5d
amingoli78 <amin.goli78@gmail.com>
2021-12-20 19:40:54 +0330

    fix messagebox widget

--- app/src/main/java/amingoli/com/selar/activity/main/MainActivity.kt
+++ app/src/main/java/amingoli/com/selar/activity/main/MainActivity.kt
@@ -4,15 +4,34 @@ import amingoli.com.selar.R
 import amingoli.com.selar.activity.product.ProductActivity
 import amingoli.com.selar.helper.App
 import amingoli.com.selar.model.Product
+import amingoli.com.selar.widget.MessageBox
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.widget_message_box.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        messageBox.setValue("هشدار","این یک پیام تست است","متوجه شدم",false,
+            object : MessageBox.Listener{
+            override fun onButtonClicked() {
+                messageBox.visibility = View.GONE
+            }
+
+            override fun onCloseClicked() {
+                messageBox.visibility = View.GONE
+            }
+
+        })
+
+        toolbar.title.text = "پیشخان فروشنده"
 
         val p = Product()
         p.name = ""
--- app/src/main/java/amingoli/com/selar/widget/MessageBox.kt
+++ app/src/main/java/amingoli/com/selar/widget/MessageBox.kt
@@ -1,4 +1,56 @@
 package amingoli.com.selar.widget
 
-class MessageBox {
+import amingoli.com.selar.R
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.RelativeLayout
+import kotlinx.android.synthetic.main.widget_message_box.view.*
+
+class MessageBox(context: Context?, attrs: AttributeSet?) : RelativeLayout(context, attrs) {
+
+    private var listener: Listener? = null
+
+    init {
+        View.inflate(context, R.layout.widget_message_box, this)
+    }
+
+    fun setValue(_title:String, content:String, buttonText: String, cancelable:Boolean, _listener: Listener?){
+
+        if (!_title.isNullOrEmpty()){
+            title.visibility = View.VISIBLE
+            title.setText(_title)
+        }
+
+        if (!content.isNullOrEmpty()){
+            desc.visibility = View.VISIBLE
+            desc.setText(content)
+        }
+
+        if (!buttonText.isNullOrEmpty()){
+            button.visibility = View.VISIBLE
+            button.setText(buttonText)
+        }
+
+        if (_listener != null){
+            this.listener = _listener
+
+            button.setOnClickListener {
+                listener?.onButtonClicked()
+            }
+
+            if (cancelable){
+                ic_close.visibility = View.VISIBLE
+                ic_close.setOnClickListener {
+                    listener?.onCloseClicked()
+                }
+            }
+        }
+
+    }
+
+    interface Listener{
+        fun onButtonClicked()
+        fun onCloseClicked()
+    }
 }
