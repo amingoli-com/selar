4d443747e608349a420979c4f2a4fcc3ded4a55d
amingoli78 <amin.goli78@gmail.com>
2021-12-22 18:20:46 +0330

    fix iamge product

---
+++ app/src/main/java/amingoli/com/selar/activity/ChooseCameraActivity.kt
@@ -0,0 +1,11 @@
+package amingoli.com.selar.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseCameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_camera)
+    }
+}
---
+++ app/src/main/java/amingoli/com/selar/dialog/PikeImageDialog.kt
@@ -0,0 +1,4 @@
+package amingoli.com.selar.dialog
+
+class PickeImageDialog {
+}
