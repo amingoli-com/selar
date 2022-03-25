510d76981fd5eab8500c9ec5ecf4b2369607a6b8
amingoli78 <amin.goli78@gmail.com>
2022-03-25 15:15:29 +0430

    add activity

---
+++ app/src/main/java/amingoli/com/selar/activity/add_order/AddOrderCameraActivity.kt
@@ -0,0 +1,11 @@
+package amingoli.com.selar.activity.add_order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddOrderCameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_order_camera)
+    }
+}
