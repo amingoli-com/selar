a1dd4261c8e6af452900fba5a5f192eb2d89353a
amingoli78 <amin.goli78@gmail.com>
2022-04-16 17:31:07 +0430

    add file

---
+++ app/src/main/java/amingoli/com/selar/activity/first_open/FirstOpenActivity.kt
@@ -0,0 +1,11 @@
+package amingoli.com.selar.activity.first_open
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstOpenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_open)
+    }
+}
