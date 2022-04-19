b90da89a5796e2257922f32b17086b9d579d336e
amingoli78 <amin.goli78@gmail.com>
2022-04-19 15:42:08 +0430

    add

---
+++ app/src/main/java/amingoli/com/selar/adapter/CustomerListAdapter.kt
@@ -0,0 +1,84 @@
+package amingoli.com.selar.adapter
+
+import amingoli.com.selar.R
+import amingoli.com.selar.model.Category
+import amingoli.com.selar.model.Product
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_category_2.view.*
+
+class CategoryListAdapter(val context: Context,
+                          val list: ArrayList<Category>,
+                          val listener: Listener
+) : RecyclerView.Adapter<CategoryListAdapter.ListViewHolder>() {
+
+    var position_selected = 0
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    interface Listener{
+        fun onItemClicked(position: Int, item: Category)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        return ListViewHolder(LayoutInflater.from(context).inflate(R.layout.item_category_2, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    @SuppressLint("SetTextI18n", "UseCompatLoadingForDrawables")
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val item = holder.itemView
+        val model = list[position]
+
+        item.title.text = model.name
+        if (!model.image.isNullOrEmpty()) {
+            item.image.visibility = View.VISIBLE
+            Glide.with(context).load(model.image!!).into(item.image)
+        }else item.image.visibility = View.GONE
+
+        item.setOnClickListener {
+            val back_position_selected = position_selected
+            position_selected = position
+            listener.onItemClicked(position,model)
+            notifyItemChanged(position_selected, model)
+            notifyItemChanged(back_position_selected, model)
+        }
+    }
+
+    fun addItem(item: Category){
+        list.add(list.size,item)
+        notifyItemInserted(list.size)
+    }
+
+    fun addItem(item: Category, position: Int){
+        if (position == -1) addItem(item)
+        else {
+            Log.e("qqq", "addItem status is pos: $position" )
+            list[position] = item
+            notifyItemChanged(position,item)
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateList(list : List<Category>){
+        for (i in list.indices){
+            Log.e("qqqq", "updateList category: "+list[i].id )
+        }
+
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
+}
