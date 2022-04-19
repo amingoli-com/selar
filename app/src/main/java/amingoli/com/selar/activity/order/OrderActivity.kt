3e7fdec8fd9c5292e6a540d7775433d8a3ba4a38
amingoli78 <amin.goli78@gmail.com>
2022-04-19 17:48:32 +0430

    add

---
+++ app/src/main/java/amingoli/com/selar/activity/order/OrderActivity.kt
@@ -0,0 +1,11 @@
+package amingoli.com.selar.activity.order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
+}
