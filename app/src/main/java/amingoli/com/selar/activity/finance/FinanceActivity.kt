64214c0b2694032562c65fe2d8f20607c0f68ca3
amingoli78 <amin.goli78@gmail.com>
2022-04-25 14:05:58 +0430

    add

---
+++ app/src/main/java/amingoli/com/selar/activity/finance/FinanceActivity.kt
@@ -0,0 +1,11 @@
+package amingoli.com.selar.activity.finance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FinanceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finance)
+    }
+}
---
+++ app/src/main/java/amingoli/com/selar/activity/stock/StockActivity.kt
@@ -0,0 +1,11 @@
+package amingoli.com.selar.activity.stock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StockActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stock)
+    }
+}
